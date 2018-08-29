65fed56e0ccd25d983652f226492c4ddd684d32c
Garner Lee <1672745+lgarner@users.noreply.github.com>
2018-08-29 16:16:51 -0700

    Initial Android EmptyMatchEngineMavenApp for Maven MEX DME SDK.

---
+++ android/EmptyMatchEngineMavenApp/README.md
@@ -0,0 +1,11 @@
+# EmptyMatchEngineMavenApp
+
+EmptyMatchEngineMaven app shows how to use the MobiledgeX SDK. The SDK is located at
+a MobiledgeX nexus maven repository, and currently requires a valid username and
+password to access SDK releases.
+
+In your app's local.properties file located in the app's root directory, add the
+following user credentials:
+
+nexus.user=mavenUsername
+nexus.password=mavenPassword
---
+++ android/EmptyMatchEngineMavenApp/app/src/main/java/com/mobiledgex/emptymatchengineapp/EnhancedLocationDialog.java
@@ -0,0 +1,39 @@
+package com.mobiledgex.emptymatchengineapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Shows OK/Cancel confirmation dialog about needed permissions.
+ */
+public class EnhancedLocationDialog extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setMessage(R.string.enchanced_location_permission)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Open this App's Location settings:
+                        Intent intent = new Intent(getActivity(), SettingsActivity.class);
+                        startActivity(intent);
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                .create();
+    }
+}
