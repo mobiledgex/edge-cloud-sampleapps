de337a7bfb348caafefd525c7ded04f47cb36674
Bruce Armstrong <41596890+brucearmstrong@users.noreply.github.com>
2018-09-07 10:08:48 -0500

    EDGECLOUD-148 EDGECLOUD-149 EDGECLOUD-153 EDGECLOUD-154 Post-demo (#5)
    * EDGECLOUD-148 EDGECLOUD-149 EDGECLOUD-153 EDGECLOUD-154 Updates for demo environment
    
    * EDGECLOUD-148 EDGECLOUD-149 EDGECLOUD-153 EDGECLOUD-154 Updates for demo environment

--- MobiledgeXSDKDemo/app/src/main/java/com/mobiledgex/sdkdemo/MatchingEngineResultsListener.java
+++ MobiledgeXSDKDemo/app/src/main/java/com/mobiledgex/sdkdemo/MatchingEngineResultsListener.java
@@ -6,7 +6,7 @@ import distributed_match_engine.AppClient;
 
 public interface MatchingEngineResultsListener {
     void onRegister(String sessionCookie);
-    void onVerifyLocation(boolean verified, double gpsLocationAccuracyKM);
+    void onVerifyLocation(AppClient.Match_Engine_Loc_Verify.GPS_Location_Status status, double gpsLocationAccuracyKM);
     void onFindCloudlet(FindCloudletResponse closestCloudlet);
     void onGetCloudletList(AppClient.Match_Engine_Cloudlet_List cloudletList);
 }
