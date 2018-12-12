84be6ba9bfb694b72fff61295b294f8a045a4bcd
Bruce Armstrong <41596890+brucearmstrong@users.noreply.github.com>
2018-12-12 10:27:03 -0600

    Fix for face recognition index problems (#27)

--- FaceDetectionServer/moedx/tracker/apps.py
+++ FaceDetectionServer/moedx/tracker/apps.py
@@ -14,6 +14,9 @@ class TrackerConfig(AppConfig):
         global myFaceRecognizer
         myFaceRecognizer = FaceRecognizer()
         logger.info("Created myFaceRecognizer")
+        logger.info("Calling update_training_data()")
+        myFaceRecognizer.update_training_data()
+        logger.info("update_training_data() complete")
+        logger.info("Calling read_trained_data()")
         myFaceRecognizer.read_trained_data()
         logger.info("read_trained_data() complete")
-        
