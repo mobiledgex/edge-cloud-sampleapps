d5c4689cf4c357c86fc006c92d7dd90e86a62064
Garner Lee <1672745+lgarner@users.noreply.github.com>
2019-01-16 14:28:11 -0800

    Move unity MexRestSample to edge-cloud-sampleapps

---
+++ unity/MexRestSample/Assets/ButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+    public Button startMexSampleButton;
+    public MexSample mexSample;
+
+    void Start () {
+        Button btn = startMexSampleButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick(){
+        Debug.Log ("You have clicked the button!");
+        mexSample.RunSampleFlow();
+    }
+
+}
---
+++ unity/MexRestSample/Assets/StatusContainer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+/* A simple data container for scrollRect content. */
+public class StatusContainer : MonoBehaviour {
+
+    public string status; // Need a list, or accessor.
+    public Text scrollViewText;
+    long count = 0;
+
+    List<string> posts = new List<string>(26);
+
+	// Use this for initialization
+	void Start () {
+       scrollViewText = GameObject.Find("/UICanvas/OutputScrollView/Viewport/Content/GRPCOutputText").GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        status = "[Sample Output]: " + count + "\n";
+        foreach (string post in posts) {
+            status += post + "\n";
+        }
+        count++;
+        scrollViewText.text = status;
+    }
+
+    public void Post(string postText) {
+        if (postText == null) {
+            return;
+        }
+        // Trim first in line if needed.
+        if (posts.Count == posts.Capacity) {
+            posts.RemoveAt(0);
+        }
+        posts.Add(postText);
+    }
+}
