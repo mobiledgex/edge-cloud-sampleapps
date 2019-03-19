c1b27d38b089fcb35bf21336d32a8c552bac3195
brucearmstrong <bruce.armstrong@mobiledgex.com>
2019-03-19 10:22:14 -0500

    Adding main menu so we can more easily add activities to app

--- android/WorkshopSkeleton/app/src/main/java/com/mobiledgex/workshopskeleton/MainActivity.java
+++ android/WorkshopSkeleton/app/src/main/java/com/mobiledgex/workshopskeleton/MainActivity.java
@@ -5,30 +5,36 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-// Matching Engine API:
-import com.google.android.gms.maps.model.LatLng;
-import com.google.maps.android.SphericalUtil;
-import com.mobiledgex.matchingengine.MatchingEngine;
-
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.SphericalUtil;
+
+// Matching Engine API:
+import com.mobiledgex.matchingengine.MatchingEngine;
 import distributed_match_engine.AppClient;
 import distributed_match_engine.Appcommon;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
     private static final String TAG = "MainActivity";
     private MatchingEngine matchingEngine;
     private String someText = null;
@@ -55,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         ctx = this;
 
@@ -90,12 +97,30 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -106,23 +131,27 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_reset) {
-            carrierNameTv.setText("none");
-            appNameTv.setText("none");
-            latitudeTv.setText("none");
-            longitudeTv.setText("none");
-            fqdnTv.setText("none");
-            portNumberTv.setText("none");
-            distanceTv.setText("none");
-            cloudletNameTv.setText("none");
-            checkboxRegistered.setChecked(false);
-            checkboxRegistered.setText(R.string.client_registered_question);
-            checkboxCloudletFound.setChecked(false);
-            checkboxCloudletFound.setText(R.string.cloudlet_found_question);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_face_recognition) {
+            // Handle the camera action
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     private boolean registerClient() throws ExecutionException, InterruptedException, io.grpc.StatusRuntimeException {
