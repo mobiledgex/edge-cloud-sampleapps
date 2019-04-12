498c65a98c71e848105432ec45ca5c9cdee6b397
Bruce Armstrong <41596890+brucearmstrong@users.noreply.github.com>
2019-04-12 16:35:31 -0500

    EDGECLOUD-554 Prep for adding Face Detection via library (#61)
    * EDGECLOUD-554 Prep for adding Face Detection via library
    
    * Removing a few things from the manifest that will be added during the workshop.
    
    * Adding Google auth dependency
    
    * Minor updates
    
    * Removing unused variable
    
    * Minor updates
    
    * Removing toggleViews() content
    
    * Get rid of white bar on top

---
+++ android/WorkshopSkeleton/app/src/main/java/com/mobiledgex/workshopskeleton/FaceProcessorFragment.java
@@ -0,0 +1,115 @@
+package com.mobiledgex.workshopskeleton;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.hardware.camera2.CameraCharacteristics;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+
+import java.text.DecimalFormat;
+
+public class FaceProcessorFragment extends Fragment {
+
+    private static final String TAG = "FaceProcessorFragment";
+    private TextView mLatencyFull;
+    private TextView mLatencyNet;
+    private TextView mStdFull;
+    private TextView mStdNet;
+    private TextView mStatusText;
+    private Toolbar mCameraToolbar;
+
+    public static FaceProcessorFragment newInstance() {
+        return new FaceProcessorFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView FaceProcessorFragment");
+        return inflater.inflate(R.layout.fragment_face_processor, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, Bundle savedInstanceState) {
+        Log.i(TAG, "onViewCreated FaceProcessorFragment savedInstanceState="+savedInstanceState);
+
+        mCameraToolbar = view.findViewById(R.id.cameraToolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(mCameraToolbar);
+
+        mLatencyFull = view.findViewById(R.id.full_latency);
+        mLatencyNet = view.findViewById(R.id.network_latency);
+        mStdFull = view.findViewById(R.id.full_std_dev);
+        mStdNet = view.findViewById(R.id.network_std_dev);
+        mStatusText = view.findViewById(R.id.statusTextView);
+
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        // TODO: Copy/paste the code to access preferences.
+
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        // TODO: Copy/paste the code to define a Camera2BasicFragment
+
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        // TODO: Copy/paste the code to define an ImageSender
+
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        // TODO: Copy/paste the code to define a FaceBoxRenderer
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // TODO: To get rid of the "Not implemented" message, hide mStatusText or set the value to an empty string.
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        Log.i(TAG, "onCreateOptionsMenu");
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.face_detection_menu, menu);
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    // TODO: Copy/paste the code to implement methods defined in the ImageProcessorInterface.
+
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    // TODO: Copy/paste the code to implement methods defined in the ImageServerInterface.
+
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    // TODO: Copy/paste the onOptionsItemSelected code to define menu selection actions.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return false;
+    }
+
+    protected void toggleViews() {
+        //We are overriding the this method just so the superclass version doesn't try to toggle
+        //the visibility of a widget we don't have in our layout.
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    // TODO: Copy/paste the getStatsText code to return actual stat values.
+    /**
+     * Return statistics from the latency measurements.
+     * @return
+     */
+    public String getStatsText() {
+        return "TODO";
+    }
+}
