72de250c5075418e5963566672e60b351a69d9b6
brucearmstrong <bruce.armstrong@mobiledgex.com>
2019-05-30 14:22:00 -0500

    EDGECLOUD-703 Updates for latest proto files

--- android/MobiledgeXSDKDemo/app/src/main/java/com/mobiledgex/sdkdemo/MatchingEngineResultsInterface.java
+++ android/MobiledgeXSDKDemo/app/src/main/java/com/mobiledgex/sdkdemo/MatchingEngineResultsInterface.java
@@ -4,7 +4,7 @@ import distributed_match_engine.AppClient;
 
 public interface MatchingEngineResultsInterface {
     void onRegister(String sessionCookie);
-    void onVerifyLocation(AppClient.VerifyLocationReply.GPS_Location_Status status, double gpsLocationAccuracyKM);
+    void onVerifyLocation(AppClient.VerifyLocationReply.GPSLocationStatus status, double gpsLocationAccuracyKM);
     void onFindCloudlet(AppClient.FindCloudletReply closestCloudlet);
     void onGetCloudletList(AppClient.AppInstListReply cloudletList);
 }
