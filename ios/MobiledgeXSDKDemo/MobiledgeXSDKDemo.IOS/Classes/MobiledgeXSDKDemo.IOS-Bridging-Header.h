aa147930b78ba53e83d3f87addf43ea40b7dcb26
Garner Lee <1672745+lgarner@users.noreply.github.com>
2019-09-12 12:12:00 -0700

    Remove obsolete ios version. In separate repo now.

--- ios/MobiledgeXSDKDemo/MobiledgeXSDKDemo.IOS/Classes/MobiledgeXSDKDemo.IOS-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-
-
-
-#import <openssl/pkcs7.h>       // JT 18.10.26
-#import <openssl/objects.h>     // JT 18.10.26
--- ios/MobiledgeXSDKDemo/MobiledgeXSDKDemo.IOS/ThirdParty/History/SwiftGRPC-master/Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import SwiftGRPCTestSuite
-
-XCTMain([
-     testCase(SwiftGRPCTests.allTests),
-])
