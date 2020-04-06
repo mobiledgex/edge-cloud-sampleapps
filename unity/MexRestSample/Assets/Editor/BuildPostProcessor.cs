e1c4196b9d294f85b83d1f339990966519d7ed7d
Ahmed Hassan <ahmedhussam.hellomobile@gmail.com>
2020-04-05 17:03:21 -0700

    Unity New Feature

---
+++ unity/MexRestSample/Assets/Editor/BuildPostProcessor.cs
@@ -0,0 +1,41 @@
+﻿
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+public class BuildPostProcessor
+{
+
+
+    [PostProcessBuildAttribute(1)]
+    public static void OnPostProcessBuild(BuildTarget target, string path)
+    {
+        if (target == BuildTarget.iOS)
+        {
+            // Read.
+            string projectPath = PBXProject.GetPBXProjectPath(path);
+            PBXProject project = new PBXProject();
+            project.ReadFromString(File.ReadAllText(projectPath));
+            string targetGUID = project.GetUnityMainTargetGuid();
+            string unityFrameworkGUID = project.GetUnityFrameworkTargetGuid();
+
+            AddFrameworks(project, targetGUID, unityFrameworkGUID);
+
+            // Write.
+            File.WriteAllText(projectPath, project.WriteToString());
+        }
+    }
+
+    static void AddFrameworks(PBXProject project, string targetGUID,string unityFrameworkGUID)
+    {
+        // to add CoreTelephonyFramwork to the project
+        project.AddFrameworkToProject(targetGUID, "CoreTelephony.framework", false);
+        // to add CoreTelephonyFramework to UnityFramework
+        project.AddFrameworkToProject(unityFrameworkGUID, "CoreTelephony.framework", false);
+
+        // Add `-ObjC` to "Other Linker Flags".
+        project.AddBuildProperty(targetGUID, "OTHER_LDFLAGS", "-ObjC");
+    }
+}
+
