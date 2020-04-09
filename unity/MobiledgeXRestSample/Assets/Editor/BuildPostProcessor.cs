3128ab3d2c7e72ecc8a372a9d8a46d54ab5d06cf
Ahmed Hassan <ahmedhussam.hellomobile@gmail.com>
2020-04-09 06:33:25 -0700

    Mex to Mobiledgex

---
+++ unity/MobiledgeXRestSample/Assets/Editor/BuildPostProcessor.cs
@@ -0,0 +1,58 @@
+﻿
+/**
+ * Copyright 2020 MobiledgeX, Inc. All rights and licenses reserved.
+ * MobiledgeX, Inc. 156 2nd Street #408, San Francisco, CA 94105
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+public class BuildPostProcessor
+{
+
+
+    [PostProcessBuildAttribute(1)]
+    public static void OnPostProcessBuild(BuildTarget target, string path)
+    {
+        if (target == BuildTarget.iOS)
+        {
+            // Read.
+            string projectPath = PBXProject.GetPBXProjectPath(path);
+            PBXProject project = new PBXProject();
+            project.ReadFromString(File.ReadAllText(projectPath));
+            string targetGUID = project.GetUnityMainTargetGuid();
+            string unityFrameworkGUID = project.GetUnityFrameworkTargetGuid();
+
+            AddFrameworks(project, targetGUID, unityFrameworkGUID);
+
+            // Write.
+            File.WriteAllText(projectPath, project.WriteToString());
+        }
+    }
+
+    static void AddFrameworks(PBXProject project, string targetGUID,string unityFrameworkGUID)
+    {
+        // to add CoreTelephonyFramwork to the project
+        project.AddFrameworkToProject(targetGUID, "CoreTelephony.framework", false);
+        // to add CoreTelephonyFramework to UnityFramework
+        project.AddFrameworkToProject(unityFrameworkGUID, "CoreTelephony.framework", false);
+
+        // Add `-ObjC` to "Other Linker Flags".
+        project.AddBuildProperty(targetGUID, "OTHER_LDFLAGS", "-ObjC");
+    }
+}
+
---
+++ unity/MobiledgeXRestSample/Assets/Plugins/iOS/PlatformIntegration.m
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2020 MobiledgeX, Inc. All rights and licenses reserved.
+ * MobiledgeX, Inc. 156 2nd Street #408, San Francisco, CA 94105
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//
+//  PlatformIntegration.m
+//  Unity-iPhone
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreTelephony/CTTelephonyNetworkInfo.h>
+#import <CoreTelephony/CTCarrier.h>
+
+
+char* convertToCStr(const char* str) {
+    if (str == NULL) {
+        return (char*)NULL;
+    }
+
+    size_t len = strlen(str);
+    char* out = (char*)malloc(len + 1);
+    strcpy(out, str);
+    return out;
+}
+
+char* _getCurrentCarrierName()
+{
+    CTTelephonyNetworkInfo *netinfo = [[CTTelephonyNetworkInfo alloc] init];
+    CTCarrier *carrier = [netinfo subscriberCellularProvider]; // s for dual SIM?
+    NSLog(@"Carrier Name: %@", [carrier carrierName]);
+    // Ref counted.
+
+    NSString* nsstr = [carrier carrierName];
+
+    return convertToCStr([nsstr UTF8String]);
+}
