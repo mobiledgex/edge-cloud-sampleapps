32bab5c8113706402b3a9752e7b5796086744841
Ahmed Schrute <ahmedhussam.hellomobile@gmail.com>
2020-06-29 15:15:02 -0700

    NetworkManager.cs -REST

---
+++ unity/ComputerVision-UnityClient/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MobiledgeX;
+using System;
+using System.Threading.Tasks;
+
+namespace MobiledgeXComputerVision {
+
+    public class NetworkManager:MonoBehaviour
+    {
+        MobiledgeXIntegration integration;
+        public AppManager appManager;
+        public enum ConnectionMode
+        {
+            Rest,
+            WebSocket
+        }
+        public static ConnectionMode connectionMode;
+
+        public async Task<string> UriBasedOnConnectionMode()
+        {
+            integration = new MobiledgeXIntegration();
+            await integration.RegisterAndFindCloudlet();
+
+            switch (connectionMode)
+            {
+                case ConnectionMode.WebSocket:
+                    throw new Exception("Not Implemented Yet");
+                default:
+                case ConnectionMode.Rest:
+                    integration.GetAppPort(DistributedMatchEngine.LProto.L_PROTO_HTTP);
+                    return integration.GetUrl("http");
+            }
+        }
+
+    }
+}
