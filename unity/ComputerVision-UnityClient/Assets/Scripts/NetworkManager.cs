b273dbf2e5799234e4964585190a7cbee380b2ea
Ahmed Schrute <ahmedhussam.hellomobile@gmail.com>
2020-06-30 17:14:48 -0700

    NetworkManager.cs Skeleton

--- unity/ComputerVision-UnityClient/Assets/Scripts/NetworkManager.cs
+++ unity/ComputerVision-UnityClient/Assets/Scripts/NetworkManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using MobiledgeX;
 using System;
 using System.Threading.Tasks;
 
@@ -9,31 +8,25 @@ namespace MobiledgeXComputerVision {
 
     public class NetworkManager:MonoBehaviour
     {
-        //MobiledgeXIntegration integration;
         public AppManager appManager;
         public enum ConnectionMode
         {
             Rest,
             WebSocket
         }
+
         public static ConnectionMode connectionMode;
 
         public async Task<string> UriBasedOnConnectionMode()
         {
-            //integration = new MobiledgeXIntegration();
-            //await integration.RegisterAndFindCloudlet();
-
             switch (connectionMode)
             {
                 case ConnectionMode.WebSocket:
                     throw new Exception("Not Implemented Yet");
-                default:
+                default: 
                 case ConnectionMode.Rest:
-                    //integration.GetAppPort(DistributedMatchEngine.LProto.L_PROTO_HTTP);
-                    //return integration.GetUrl("http");
                     throw new Exception("Add Mobiledgex package : https://github.com/mobiledgex/edge-cloud-sdk-unity ");
             }
         }
-
     }
 }
