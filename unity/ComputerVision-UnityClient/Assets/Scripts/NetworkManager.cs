c4cea07d404ffaa1c9b2c8df1058bbe3c401da0d
Ahmed Schrute <ahmedhussam.hellomobile@gmail.com>
2020-07-02 17:29:29 -0700

    NetworkManager.cs WebSocket Support

--- unity/ComputerVision-UnityClient/Assets/Scripts/NetworkManager.cs
+++ unity/ComputerVision-UnityClient/Assets/Scripts/NetworkManager.cs
@@ -1,31 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Threading.Tasks;
-
+using System.Net;
+using System.Net.Sockets;
+using MobiledgeX;
 namespace MobiledgeXComputerVision {
 
     public class NetworkManager:MonoBehaviour
     {
         public AppManager appManager;
+        MobiledgeXWebSocketClient client;
+        MobiledgeXIntegration integration;
         public enum ConnectionMode
         {
             Rest,
             WebSocket
         }
 
-        public static ConnectionMode connectionMode;
+        public ConnectionMode connectionMode;
 
         public async Task<string> UriBasedOnConnectionMode()
         {
+            DistributedMatchEngine.AppPort appPort;
+            string url;
             switch (connectionMode)
             {
                 case ConnectionMode.WebSocket:
-                    throw new Exception("Not Implemented Yet");
-                default: 
+                    integration = new MobiledgeXIntegration();
+                    await integration.RegisterAndFindCloudlet();
+                    appPort = integration.GetAppPort(DistributedMatchEngine.LProto.L_PROTO_TCP);
+                    url = integration.GetUrl("ws");
+                    return url;
                 case ConnectionMode.Rest:
-                    throw new Exception("Add Mobiledgex package : https://github.com/mobiledgex/edge-cloud-sdk-unity ");
+                    integration = new MobiledgeXIntegration();
+                    await integration.RegisterAndFindCloudlet();
+                    appPort = integration.GetAppPort(DistributedMatchEngine.LProto.L_PROTO_TCP);
+                    url = integration.GetUrl("http");
+                    return url;
+                default:
+                    return "";
+            }
+        }
+
+        public async void StartWs(string url)
+        {
+            Uri uri = new Uri(url);
+            client = new MobiledgeXWebSocketClient(System.Net.WebSockets.WebSocketMessageType.Binary);
+            if (client.isOpen())
+            {
+                client.Dispose();
+
+                client = new MobiledgeXWebSocketClient(System.Net.WebSockets.WebSocketMessageType.Binary);
+            }
+            await client.Connect(uri);
+            appManager.wsStarted = true;
+        }
+
+        public void SendtoServer(byte[] imgBinary)
+        {
+            client.Send(imgBinary);
+        }
+
+        private void Update()
+        {
+            if (client == null)
+            {
+                return;
+            }
+            var cqueue = client.receiveQueue;
+            string msg;
+            while (cqueue.TryPeek(out msg))
+            {
+                cqueue.TryDequeue(out msg);
+                print("Dequeued this message: " + msg);
+                appManager.HandleServerRespone(msg);
             }
         }
     }
