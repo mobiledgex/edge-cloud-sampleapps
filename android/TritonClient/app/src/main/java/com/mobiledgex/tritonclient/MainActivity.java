3f659bcc9f4c850c1b45ef600cba104f201d371e
brucearmstrong <bruce.armstrong@mobiledgex.com>
2021-07-01 14:03:33 -0500

    Removing unused floating action button

--- android/TritonClient/app/src/main/java/com/mobiledgex/tritonclient/MainActivity.java
+++ android/TritonClient/app/src/main/java/com/mobiledgex/tritonclient/MainActivity.java
@@ -76,15 +76,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        fab.setVisibility(View.GONE); //TODO: Figure out if we need this button.
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
