3abfc883f4686b264a5625aadd9f9ec277ae65e2
Bruce Armstrong <41596890+brucearmstrong@users.noreply.github.com>
2022-03-03 11:59:20 -0600

    EDGECLOUD-6183 Remove Persistent TCP Server support (#297)

--- ComputerVisionServer/moedx/tracker/routing.py
+++ ComputerVisionServer/moedx/tracker/routing.py
@@ -3,8 +3,8 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path('ws/detector/detect', consumers.ImageConsumerFaceDetector),
-    re_path('ws/recognizer/predict', consumers.ImageConsumerFaceRecognizer),
-    re_path('ws/openpose/detect', consumers.ImageConsumerOpenposeDetector),
-    re_path('ws/object/detect', consumers.ImageConsumerObjectDetector),
+    re_path('ws/detector/detect', consumers.ImageConsumerFaceDetector.as_asgi()),
+    re_path('ws/recognizer/predict', consumers.ImageConsumerFaceRecognizer.as_asgi()),
+    re_path('ws/openpose/detect', consumers.ImageConsumerOpenposeDetector.as_asgi()),
+    re_path('ws/object/detect', consumers.ImageConsumerObjectDetector.as_asgi()),
 ]
